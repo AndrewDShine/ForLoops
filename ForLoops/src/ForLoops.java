32fa422df33df7cafd09ccfdb487b2d51326d076
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2018-08-27 08:56:18 -0600

    Finished through challenge 5

--- ForLoops/src/ForLoops.java
+++ ForLoops/src/ForLoops.java
@@ -10,7 +10,34 @@ public class ForLoops
 						//change
 						System.out.println("Which challenge (1-8) would you like to see?");
 						int challengeNumber = userInput.nextInt();
-						showGeniusFour();
+						switch (challengeNumber)
+						{
+							case 1:
+								showGeniusOne();
+								break;
+							case 2:
+								showGeniusTwo();
+								break;
+							case 3:
+								showGeniusThree();
+								break;
+							case 4:
+								showGeniusFour();
+								break;
+							case 5:
+								showGeniusFive();
+								break;
+							case 6:
+								showGeniusSix();
+								break;
+							case 7:
+								showGeniusSeven();
+								break;
+							case 8:
+								showGeniusEight();
+								break;
+							
+						}
 						System.out.print("\n");
 					}
 				while (true);
@@ -47,11 +74,22 @@ public class ForLoops
 		}
 		public static void showGeniusFive()
 		{
-			
+			int total = 0;
+			for (int i = 5; i <= 100; i = i + 5)
+				{
+					total = total + i;
+				}
+			System.out.println(total);
 		}
 		public static void showGeniusSix()
 		{
+			Scanner userBounds = new Scanner(System.in);
+			System.out.println("Hi, user! Enter a lower bound.");
+			int lowerBound = userBounds.nextInt();
+			System.out.println("Okay, now enter an upper bound!");
+			int upperBound = userBounds.nextInt();
 			
+			for ()
 		}
 		public static void showGeniusSeven()
 		{
